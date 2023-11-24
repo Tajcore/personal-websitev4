96bc7d9886da4229500d3b1a4863fbe483932386
Tahjyei Thompson <tahjyeithompson@gmail.com>
2023-11-24 18:53:34 -0500

    Update site header styling

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -8,7 +8,7 @@ import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
   return (
-    <header className="mb-24 flex h-full lg:sticky lg:top-0 lg:mb-0 lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+    <header className="mb-44 flex h-full lg:sticky lg:top-0 lg:mb-0 lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
       <div className="container flex h-16 flex-col items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
